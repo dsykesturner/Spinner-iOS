e5dd31ab529e1e1559cc99c045d86ad48a05671d
Daniel Sykes-Turner <dst@localz.co>
2017-11-05 14:04:30 +1100

    Release 1.1.1 (iPhone X support)

---
+++ Spinner/SettingsTextSizeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTextSizeTableViewCell.swift
+//  Spinner
+//
+//  Created by Daniel Sykes-Turner on 24/9/16.
+//  Copyright © 2016 UniverseApps. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTextSizeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
